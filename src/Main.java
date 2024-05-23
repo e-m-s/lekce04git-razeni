a656336b30bd56d6570eb147ca2e243533ae856b
Martin Simunek <public@e-mis.cz>
2024-05-23 20:10:29 +0200

    add sorting demo
    for computers - still not working
    for integers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import com.engeto.computers.Computer;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        sortComputers();
+        sortIntegers();
     }
+
+    private static void sortComputers() {
+        List<Computer> computers = new ArrayList<>();
+        computers.add(new Computer("Xyz", LocalDate.of(2020, 1, 1), 1.5));
+        computers.add(new Computer("Abc", LocalDate.of(2019, 1, 1), 2.5));
+        computers.add(new Computer("Def", LocalDate.of(2018, 1, 1), 2.0));
+
+        // Nelze - nevím, podle čeho řadit: Collections.sort(computers);
+        //computers.sort();
+    }
+
+    private static void sortIntegers() {
+        List<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        integers.add(3);
+        integers.add(2);
+
+        System.out.println("Před seřazením: " + integers);
+        Collections.sort(integers);
+        System.out.println("Po seřazení: " + integers);
+    }
+
 }
